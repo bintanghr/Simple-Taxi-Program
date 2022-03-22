c2ddd2677b2835a1b5c40fc0e08cd25a62500b1a
bintanghr <haridaramadhan@gmail.com>
2022-03-22 21:49:51 +0700

    WIP program taxi sederhana

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTaxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = "Aldebaran";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 8;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
