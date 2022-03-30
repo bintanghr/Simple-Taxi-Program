2a0314f5a27781c78a34fce7d435a1256bbf1d76
bintanghr <haridaramadhan@gmail.com>
2022-03-31 06:53:38 +0700

    Mengubah menjadi input dinamis

--- Program.cs
+++ Program.cs
@@ -14,14 +14,27 @@ namespace ProgramTaxi
             Taxi taxi = new Taxi();
 
             //pengesetan nilai properties
-            taxi.DriverName = "Aldebaran";
-            taxi.OnDuty = true;
-            taxi.NumPassenger = 8;
+            Console.Write("Inputkan Nama Driver : ");
+            taxi.DriverName = Console.ReadLine();
+            Console.Write("On Duty [Yes or No] : ");
+            taxi.OnDuty = Console.ReadLine().ToLower() == "yes" ? true : false;
 
-            //pemanggilan method
-            taxi.TaxiInfo();
-            taxi.PickUpPassenger();
-            taxi.DropOffPassenger();
+            if (taxi.OnDuty)
+            {
+                Console.Write("Inputkan jumlah penumpang : ");
+                taxi.NumPassenger = int.Parse(Console.ReadLine());
+
+                //pemanggilan method
+                Console.WriteLine("");
+                taxi.TaxiInfo();
+                taxi.PickUpPassenger();
+                taxi.DropOffPassenger();
+            }
+            else
+            {
+                Console.WriteLine("");
+                taxi.TaxiInfo();
+            }
 
             Console.ReadKey();
         }
